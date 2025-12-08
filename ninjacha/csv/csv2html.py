5d77e3830fb0f96422ca2488afbf9ec117c3c4f9
trianguloscabri <trianguloscabri@outlook.es>
2025-12-08 23:12:04 +0100

    webpage con herramientas para aprendizaje de chino

---
+++ ninjacha/csv/csv2html.py
@@ -0,0 +1,31 @@
+import csv
+
+# Ejecutar como: python3 csv2html.py > salida.html
+
+
+# Nombre del archivo CSV de entrada
+archivo_csv = "ninjacha150.csv"
+
+# Leemos el CSV y generamos el HTML
+html_salida = []
+
+with open(archivo_csv, encoding="utf-8") as f:
+    lector = csv.reader(f)
+    for fila in lector:
+        if len(fila) != 3:
+            continue  # Saltar líneas incorrectas
+
+        chino, pinyin, traduccion = fila
+
+        bloque = (
+            f'<div class="word_or_phrase">'
+            f'<span class="pinyin"> {pinyin} </span>'
+            f'<span class="chino">{chino}</span>'
+            f'<span class="trad">{traduccion}</span>'
+            f'</div>'
+        )
+
+        html_salida.append(bloque)
+
+# Imprimir el resultado HTML
+print("\n".join(html_salida))
